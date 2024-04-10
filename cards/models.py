60b5548435c36c11351a4b20f8502aa337d74c1a
SlLVlNHA <silvinha@ciencias.unam.mx>
2024-04-09 21:14:49 -0600

    cambios en el tablero

--- cards/models.py
+++ cards/models.py
@@ -8,12 +8,14 @@ COLOR_STATUS = (
     (1,'poppy'), #FF420E
     (2,'stem'), #80BD9E
     (3,'green'), #89DA59
+    (4,'azul'),
+    (5,'rojo'),
 )
 
 class Card(models.Model):
     """Tarjetas"""
     title=models.CharField(max_length=50)
-    content=models.CharField(max_length=300)
+    content=models.CharField(max_length=200)
     color=models.IntegerField(choices=COLOR_STATUS)
     date=models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
