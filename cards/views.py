28e27f0d8984ea422052751bd4114f974bdf4197
abraham jimenez reyes <74029995+jimenezreyes@users.noreply.github.com>
2024-04-10 16:47:30 -0600

    Update views.py

--- cards/views.py
+++ cards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Card
 
 from django.views import View
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
 
 # Create your views here.
 class CardListView(ListView):
@@ -12,3 +12,8 @@ class CardListView(ListView):
 class CardDetailView(DetailView):
     model = Card
     context_object_name = 'card'
+
+class CardDeleteView(DeleteView):
+    model = Card
+    success_url = '/tablero/cards/''/cards/'  # URL a la que se redirigirá después de eliminar correctamente la nota
+    template_name = 'cards/delete_card.html'  # Plantilla que se utilizará para mostrar la confirmación de eliminación
