f64857c6667ab01036410ff30d6c90879d217906
SlLVlNHA <silvinha@ciencias.unam.mx>
2024-04-12 20:39:58 -0600

    regresando a las tarjetas rectangulares

--- cards/urls.py
+++ cards/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cards/',views.CardListView.as_view(),name='tablero'),
-    path('cards/<int:pk>',views.CardDetailView.as_view()),
-    path('cards/<int:pk>/delete/', views.CardDeleteView.as_view(), name='delete_card'), #Abraham
+    path('',views.CardListView.as_view(),name='tablero'),
+    path('delete/<int:pk>', views.CardDeleteView.as_view(), name='delete_card'), #Abraham
     path('search-blogs/',views.BlogSearchView.as_view(), name='search_blogs'), #Mau
 ]
