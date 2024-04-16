2add4abd80ef26283e8c699289bb79f02fd76bf4
AlexZbp <42795161+AlexZbp@users.noreply.github.com>
2024-04-16 11:00:42 -0600

    Cambios para editar
    Se añade la opción de editar y un botón para regresar en caso de no querer realizar la acción

--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import Card
-
+from django.db.models import Q
 from django.views import View
-from django.views.generic import ListView, DetailView
+from .forms import CardForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
+
+#Creacion de una nueva nota
+class CardCreateView(LoginRequiredMixin, CreateView):
+    model = Card
+    form_class = CardForm
+    success_url = '/tablero/'  
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  
+        return super().form_valid(form)
 
 # Create your views here.
 class CardListView(ListView):
@@ -13,3 +25,28 @@ class CardListView(ListView):
 class CardDetailView(DetailView):
     model = Card
     context_object_name = 'card'
+
+class CardDeleteView(DeleteView):
+    model = Card
+    success_url = '/tablero/'  # URL a la que se redirigirá después de eliminar correctamente la nota
+    template_name = 'cards/delete_card.html'  # Plantilla que se utilizará para mostrar la confirmación de eliminación
+
+class CardUpdateView(UpdateView):
+    model = Card
+    form_class = CardForm
+    success_url = '/tablero/'
+#Mau
+class BlogSearchView(ListView):
+    model = Card
+    template_name = 'card_list.html'
+    context_object_name = 'cards'
+    paginate_by = 6
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+
+        if query:
+            queryset = queryset.filter(Q(title__icontains=query) | Q(content__icontains=query))
+
+        return queryset
